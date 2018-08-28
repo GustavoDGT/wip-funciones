5f3ab9d1e4c720cddc1489a487bdb21a77c1a276
GustavoDGT <gustavo@dgtalliance.com>
2018-08-28 18:18:21 -0500

    wip-funciones

--- js/wip-funciones.js
+++ js/wip-funciones.js
@@ -196,9 +196,4 @@ jQuery(document).ready(function($) {
 			}
 		}
 	}
-
-	if( $("#wip-contact").length > 0 ) {
-		var scene = document.getElementById('wip-contact');
-		var parallaxInstance = new Parallax(scene);
-	}
 });
