1612f67a1ae8a0d7bae8d36b924dfcef65793c6e
GustavoDGT <gustavo@dgtalliance.com>
2018-10-02 12:53:29 -0500

    sponsors not

--- front/parts/sponsors.php
+++ front/parts/sponsors.php
@@ -7,12 +7,12 @@ $upload_path = $uploads['baseurl'];
 ?>
   <div id="its-sponsors" class="its-clients slider-home owl-carousel owl-theme">
     <div class="sponsor item">
-      <img src="<?php echo $upload_path . '/2018/08/9_sap_partner_open.png'; ?>" src="<?php echo $style_dir . '/images/whole.png'; ?>" alt="" class="client-item"/>
+      <img src="<?php echo $upload_path . '/2018/08/9_sap_partner_open.png'; ?>" alt="" class="client-item"/>
     </div>
     <div class="sponsor item">
-      <img src="<?php echo $upload_path . '/2018/07/10_peru_contable.png'; ?>" src="<?php echo $style_dir . '/images/whole.png'; ?>" alt="" class="client-item"/>
+      <img src="<?php echo $upload_path . '/2018/07/10_peru_contable.png'; ?>" alt="" class="client-item"/>
     </div>
     <div class="sponsor item">
-      <img src="<?php echo $upload_path . '/2018/08/11_camara_comercio.png'; ?>" src="<?php echo $style_dir . '/images/whole.png'; ?>" alt="" class="client-item"/>
+      <img src="<?php echo $upload_path . '/2018/08/11_camara_comercio.png'; ?>" alt="" class="client-item"/>
     </div>
   </div>
